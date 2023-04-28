7679fae748607c658c7dd2291f485f29d0abcd3c
Conrad Rolf <williamconradrolf@gmail.com>
2023-04-28 16:28:43 -0500

    Added comments to more of my code to describe it, also finished up adding in a new pair of shoes to shoeCount

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -11,18 +11,28 @@ console.log('***** Object Practice *****')
 const me = {
   // TODO - add properties here
   firstName: 'Conrad',
+  // creating my first name
   lastName: 'Rolf',
+  //creating my last name
   hasSiblings: true,
+  // setting the boolean to true because I have siblings
   shoeCount: 7,
+  // setting my shoe count
   favThreeFoods: ['sushi', 'burritos', 'curry'],
+  // creating an array of my favorite foods
   // Question #2
   name: function() {
     return this.firstName + ' ' + this.lastName;
   }
+  // creating a function to take the firstName and lastName values
+  // from within the object to create fullName outside of it
 };
 console.log('A little about me:', me);
 fullName = me.name();
+// creating the var fullName with a value equal to the 'name' function
+// within the object 'me'
 console.log(fullName)
+// making sure the function is working properly
 
 /* 2. Accessing object properties.
   - Create a variable called fullName 
@@ -39,7 +49,9 @@ console.log(fullName)
   - Console.log your last favorite food 
 */
 console.log(me.favThreeFoods[0]);
-console.log(me.favThreeFoods[2]);
+// we are accessing the first value of the array
+console.log(me.favThreeFoods[me.favThreeFoods.length - 1]);
+// here we are accessing the last value of the array
 
 
 
@@ -49,6 +61,13 @@ console.log(me.favThreeFoods[2]);
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
+console.log(me.shoeCount);
+// checking our shoe count before the increase
+me.shoeCount = (me.shoeCount + 1);
+// we are accessing the shoe count value within the object
+// and adding in a new pair of shoes
+console.log(me.shoeCount);
+// making sure that our increase adjusted the shoe count correctly
 
 
 /* 5. Add a new property to an existing object.
